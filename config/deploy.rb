4cfdaf7cf8f2a0ab08dd06b9d8a43de6bf68dc77
Matthew Landauer <mlandauer@gmail.com>
2018-03-07 16:59:47 +1100

    We allow a fresh checkout of the theme during every deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -74,8 +74,7 @@ namespace :deploy do
       "#{release_path}/cache" => "#{shared_path}/cache",
       "#{release_path}/log" => "#{shared_path}/log",
       "#{release_path}/tmp/pids" => "#{shared_path}/tmp/pids",
-      "#{release_path}/lib/acts_as_xapian/xapiandbs" => "#{shared_path}/xapiandbs",
-      "#{release_path}/lib/themes" => "#{shared_path}/themes",
+      "#{release_path}/lib/acts_as_xapian/xapiandbs" => "#{shared_path}/xapiandbs"
     }
 
     if rbenv_ruby_version
